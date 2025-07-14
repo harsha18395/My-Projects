772f649d31a1cab91505a68fc8afc63cd73936c2
Harsha Bhat <shreeharsha.bhat@segulagrp.de>
2025-07-14 21:32:02 +0200

    Initial setup and commit. Project Movielist using React

---
+++ MovieSuggestions/Readme.txt
@@ -0,0 +1,4 @@
+This project is done using React.js
+
+Required:
+-Download Node.js and use 'vite' to configure the project to use React
---
+++ MovieSuggestions/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Home from './pages/Home.jsx'
+
+function App() {
+  
+  return(
+      <>
+      <Home />        
+      </>
+  )
+}
+
+function Text({display})
+{
+  return(<div>{display}</div>)
+}
+
+export default App
